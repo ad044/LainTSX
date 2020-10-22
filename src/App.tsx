3d62868756a942d47665a8a6ada97a1d80d56abf
ad044 <antidepressant044@protonmail.com>
2020-10-22 16:06:05 +0400

    added cross thing, working on proper media time tracking

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,7 @@ const App = () => {
           {/*<MainScene />*/}
         </Canvas>
       </span>
-      {/*<MediaPlayer />*/}
+      <MediaPlayer />
     </div>
   );
 };
--- src/components/MediaScene/LeftSide/LeftSide.tsx
+++ src/components/MediaScene/LeftSide/LeftSide.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import TriangularPrism from "./TriangularPrism";
 import Cube from "./Cube";
 
