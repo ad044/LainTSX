01121c932a5d0176318ac32725236230847a2bbb
ad044 <antidepressant044@protonmail.com>
2020-11-06 00:08:15 +0400

    working on mirror texture thing

--- src/components/GateScene/GateMiddleObject/Mirror.tsx
+++ src/components/GateScene/GateMiddleObject/Mirror.tsx
@@ -1,35 +1,59 @@
 import React, { useMemo, useRef } from "react";
-import gateMirrorTexture from "../../../static/sprite/gate_object_texture.png";
 import { useFrame, useLoader } from "react-three-fiber";
 import * as THREE from "three";
 
 const Mirror = () => {
-  const gateMirrorTex = useLoader(THREE.TextureLoader, gateMirrorTexture);
+
+  const cubeTexture = useLoader(
+    // @ts-ignore
+    THREE.CubeTextureLoader,
+    [
+      "gate_object_texture_2.png",
+      "gate_object_texture_2.png",
+      "gate_object_texture_2.png",
+      "gate_object_texture_2.png",
+      "gate_object_texture_2.png",
+      "gate_object_texture_2.png",
+      "gate_object_texture_2.png",
+    ],
+    (loader: THREE.CubeTextureLoader) => loader.setPath("../../../static")
+  );
+
+  const mirrorMaterial = useMemo(() => {
+    const loader = new THREE.CubeTextureLoader();
+    const texture = loader.load([
+      "gate_object_texture.png",
+      "gate_object_texture.png",
+      "gate_object_texture.png",
+      "gate_object_texture.png",
+      "gate_object_texture.png",
+      "gate_object_texture.png",
+    ]);
+    return new THREE.MeshBasicMaterial({
+      envMap: cubeTexture,
+      depthTest: false,
+      transparent: true,
+    });
+  }, []);
+
   const mirrorRef = useRef<THREE.Object3D>();
-
-  const textureMemo = useMemo(() => {
-    gateMirrorTex.wrapS = THREE.RepeatWrapping;
-    gateMirrorTex.wrapT = THREE.RepeatWrapping;
-
-    return gateMirrorTex;
-  }, [gateMirrorTex]);
 
   useFrame(() => {
     if (mirrorRef.current) {
       mirrorRef.current.rotation.y += 0.03;
     }
+    mirrorMaterial.needsUpdate = true;
   });
 
   return (
     <group ref={mirrorRef} position={[-0.1, -0.2, -0.2]}>
-      <mesh renderOrder={4} position={[0.2, 0.2, 0.2]} scale={[0.4, 1, 0.05]}>
+      <mesh
+        renderOrder={4}
+        position={[0.2, 0.2, 0.2]}
+        scale={[0.4, 1, 0.05]}
+        material={mirrorMaterial}
+      >
         <boxBufferGeometry attach="geometry"></boxBufferGeometry>
-        <meshBasicMaterial
-          attach="material"
-          map={textureMemo}
-          transparent={true}
-          depthTest={false}
-        ></meshBasicMaterial>
       </mesh>
     </group>
   );
