dda59d70516af59d93cac62b967409ba6cd8f427
ad044 <antidepressant044@protonmail.com>
2020-12-03 19:52:07 +0400

    basic animation done! gotta clean up and add some more details

--- src/components/MainScene/PauseSquare.tsx
+++ src/components/MainScene/PauseSquare.tsx
@@ -3,12 +3,14 @@ import * as THREE from "three";
 import { useLoader } from "react-three-fiber";
 import pauseGrayBoxes from "../../static/sprite/pause_gray_boxes.png";
 import { a, useSpring } from "@react-spring/three";
+import { CurveUtils } from "three";
 
 type PauseSquareProps = {
   colIdx: number;
   rowIdx: number;
   geometry: THREE.PlaneBufferGeometry;
-  active: boolean;
+  active?: boolean;
+  shouldDisappear?: boolean;
 };
 
 const PauseSquare = (props: PauseSquareProps) => {
@@ -30,37 +32,37 @@ const PauseSquare = (props: PauseSquareProps) => {
   const rotX = toggle.to([0, 1], [-Math.PI, 0]);
   const rotY = toggle.to([0, 1], [Math.PI / 2, 0]);
 
-  const introRotX = introToggle.to([0, 1], [0, 2 * Math.PI]);
-  const introRotY = introToggle.to([0, 1], [0, 2 * Math.PI]);
+  const introRotX = introToggle.to([0, 1], [Math.PI, 0]);
+  const introRotY = introToggle.to(
+    [0, 1],
+    [Math.PI, props.shouldDisappear ? Math.PI / 2 : 0]
+  );
 
   useEffect(() => {
     setTimeout(() => {
-      setIntroAnimToggle(true);
-    }, (props.rowIdx + props.colIdx) * 100);
+      setTimeout(() => {
+        setIntroAnimToggle(true);
+      }, (props.rowIdx + props.colIdx) * 100);
 
-    setTimeout(() => {
-      setIntro(false);
-    }, 5000);
+      setTimeout(() => {
+        setIntro(false);
+      }, 1500);
+    }, 500);
   }, [props.colIdx, props.rowIdx]);
 
-  const side = useMemo(() => {
-    if (intro) return THREE.DoubleSide;
-    else {
-      if (
-        (props.colIdx > 3 && props.rowIdx <= 3) ||
-        (props.colIdx <= 3 && props.rowIdx > 3)
-      ) {
-        return THREE.FrontSide;
-      } else {
-        return THREE.BackSide;
-      }
-    }
-  }, [intro, props.colIdx, props.rowIdx]);
+  const initialState = useSpring({
+    posX: props.colIdx / 2.8,
+    posY: props.rowIdx / 2.8,
+    rotZ: 0,
+    from: { posX: 1.05, posY: 1.07, rotZ: -1 },
+    config: { duration: 500 },
+  });
 
   return (
     <a.mesh
       geometry={props.geometry}
-      position={[props.colIdx / 2.8, props.rowIdx / 2.8, 0]}
+      position-x={initialState.posX}
+      position-y={initialState.posY}
       scale={[
         props.colIdx > 3 ? -0.25 : 0.25,
         props.rowIdx <= 3 ? -0.25 : 0.25,
@@ -68,6 +70,7 @@ const PauseSquare = (props: PauseSquareProps) => {
       ]}
       rotation-x={intro ? introRotX : rotX}
       rotation-y={intro ? introRotY : rotY}
+      rotation-z={initialState.rotZ}
     >
       <meshBasicMaterial
         attach="material"
