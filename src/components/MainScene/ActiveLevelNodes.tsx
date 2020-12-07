3a76c4d75d8a6fc4e76458f7b3ccdcf23bd527db
ad044 <antidepressant044@protonmail.com>
2020-12-07 20:23:57 +0400

    added checkers for unlocked nodes

--- src/components/MainScene/ActiveLevelNodes.tsx
+++ src/components/MainScene/ActiveLevelNodes.tsx
@@ -6,6 +6,8 @@ import { useNodeStore, useLevelStore, useSiteStore } from "../../store";
 import { a, useSpring } from "@react-spring/three";
 
 const ActiveLevelNodes = () => {
+  const unlockedNodes = useNodeStore((state) => state.unlockedNodes);
+
   const activeNodeState = useNodeStore((state) => state.activeNodeState);
   const activeLevel = useLevelStore((state) => state.activeLevel);
 
@@ -30,22 +32,35 @@ const ActiveLevelNodes = () => {
       rotation-x={siteState.siteRotX}
     >
       {Object.entries(activeLevelNodes).map((node: [string, any]) => {
-        return (
-          <Node
-            sprite={node[1].node_name}
-            position={
-              node_positions[node[0].substr(2) as keyof typeof node_positions]
-                .position
-            }
-            rotation={
-              node_positions[node[0].substr(2) as keyof typeof node_positions]
-                .rotation
-            }
-            key={node[1].node_name}
-            active={node[0] === activeNodeState.id}
-            level={node[0].substr(0, 2)}
-          />
-        );
+        const unlockedBy = node[1].unlocked_by;
+
+        let unlocked;
+        if (unlockedBy === "-1") unlocked = true;
+        else
+          unlocked =
+            unlockedNodes[unlockedBy as keyof typeof unlockedNodes].unlocked;
+
+        if (
+          unlocked &&
+          (node[1].is_hidden === "0" || node[1].is_hidden === "3")
+        ) {
+          return (
+            <Node
+              sprite={node[1].node_name}
+              position={
+                node_positions[node[0].substr(2) as keyof typeof node_positions]
+                  .position
+              }
+              rotation={
+                node_positions[node[0].substr(2) as keyof typeof node_positions]
+                  .rotation
+              }
+              key={node[1].node_name}
+              active={node[0] === activeNodeState.id}
+              level={node[0].substr(0, 2)}
+            />
+          );
+        }
       })}
     </a.group>
   );
