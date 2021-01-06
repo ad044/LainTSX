715c924cdef835c7b42f5e03c2aa7e141310ceaf
ad044 <antidepressant044@protonmail.com>
2021-01-06 21:48:39 +0400

    explosion anim line poses mostly done, need some tweaking

--- src/components/MainScene/SyncedComponents/Site/NodeExplosion.tsx
+++ src/components/MainScene/SyncedComponents/Site/NodeExplosion.tsx
@@ -1,10 +1,43 @@
-import React, { useMemo } from "react";
-import { a } from "@react-spring/three";
-import * as THREE from "three";
+import React, { useMemo, useRef, useState } from "react";
 import ExplosionLine from "./NodeExplosion/ExplosionLine";
+import node_explosion_line_positions from "../../../../resources/node_explosion_line_positions.json";
+import { useFrame } from "react-three-fiber";
 
 const NodeExplosion = () => {
-  return <ExplosionLine rotation={[0, 0, -0.3]} color={"#f5cc16"} length={1} />;
+  const [currentFrame, setCurrentFrame] = useState(1);
+
+  const linePoses = useMemo(
+    () =>
+      node_explosion_line_positions[
+        currentFrame.toString() as keyof typeof node_explosion_line_positions
+      ],
+    [currentFrame]
+  );
+
+  const lastTime = useRef(0);
+
+  useFrame(() => {
+    const now = Date.now();
+    if (now > lastTime.current + 2400) {
+      if (currentFrame < 6) {
+        setCurrentFrame(currentFrame + 1);
+        lastTime.current = now;
+      }
+    }
+  });
+
+  return (
+    <group position={[-0.5, 0.45, 0]}>
+      {Object.values(linePoses).map((entry) => (
+        <ExplosionLine
+          rotation={entry.rotation as [number, number, number]}
+          position={entry.position as [number, number, number]}
+          color={entry.color}
+          length={entry.length}
+        />
+      ))}
+    </group>
+  );
 };
 
 export default NodeExplosion;
