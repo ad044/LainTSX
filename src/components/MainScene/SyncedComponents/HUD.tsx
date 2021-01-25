abbd510941b0fd10dbffd693b20fa68c2947b202
ad044 <antidepressant044@protonmail.com>
2021-01-25 21:07:59 +0400

    optimized hud a bit more

--- src/components/MainScene/SyncedComponents/HUD.tsx
+++ src/components/MainScene/SyncedComponents/HUD.tsx
@@ -1,15 +1,15 @@
-import React, { useMemo, memo } from "react";
-import { useLoader } from "react-three-fiber";
+import React, { memo, useEffect, useRef, useState } from "react";
+import { useFrame, useLoader } from "react-three-fiber";
 import * as THREE from "three";
 import bigHud from "../../../static/sprite/big_hud.png";
-import bigHudMirrored from "../../../static/sprite/big_hud_mirrored.png";
 import longHud from "../../../static/sprite/long_hud.png";
-import longHudMirrored from "../../../static/sprite/long_hud_mirrored.png";
 import boringHud from "../../../static/sprite/long_hud_boring.png";
-import boringHudMirrored from "../../../static/sprite/long_hud_boring_mirrored.png";
-import { a, useSpring } from "@react-spring/three";
+import { a } from "@react-spring/three";
 import { useStore } from "../../../store";
-import MediumLetter from "../../TextRenderer/MediumLetter";
+import { getNodeHud } from "../../../core/nodeSelector";
+import lerp from "../../../core/utils/lerp";
+import GreenTextRenderer from "../../TextRenderer/GreenTextRenderer";
+import MediaSceneEventManager from "../../../core/StateManagers/MediaSceneEventManager";
 
 export type HUDType = {
   mirrored: number;
@@ -33,124 +33,153 @@ export type HUDType = {
 };
 
 const HUD = memo(() => {
-  const greenText = useStore((state) =>
-    state.activeNode.title.split("")
+  const greenText = ["d", "i"];
+  const activeRef = useRef(true);
+  const currentHudRef = useRef(
+    getNodeHud(useStore.getState().activeNodeMatrixIndices)
   );
-  const active = useStore((state) => Number(state.hudActive));
-  const currentHud = useStore((state) => state.hud);
+  const activeNodeMatrixIndices = useStore(
+    (state) => state.activeNodeMatrixIndices
+  );
 
-  const hudElementState = useSpring({
-    bigHUDPositionX: active,
-    longHUDPositionX: active,
-    boringHUDPositionX: active,
-    greenTextPositionX: active,
-    config: { duration: 400 },
+  useFrame(() => {
+    if (
+      longHudRef.current &&
+      bigHudRef.current &&
+      boringHudRef.current &&
+      greenTextRef.current
+    ) {
+      longHudRef.current.position.x = lerp(
+        longHudRef.current.position.x,
+        !activeRef.current
+          ? currentHudRef.current.long.initial_position[0]
+          : currentHudRef.current.long.position[0],
+        0.12
+      );
+
+      boringHudRef.current.position.x = lerp(
+        boringHudRef.current.position.x,
+        !activeRef.current
+          ? currentHudRef.current.boring.initial_position[0]
+          : currentHudRef.current.boring.position[0],
+        0.12
+      );
+
+      bigHudRef.current.position.x = lerp(
+        bigHudRef.current.position.x,
+        !activeRef.current
+          ? currentHudRef.current.big.initial_position[0]
+          : currentHudRef.current.big.position[0],
+        0.12
+      );
+
+      greenTextRef.current.position.x = lerp(
+        greenTextRef.current.position.x,
+        !activeRef.current
+          ? currentHudRef.current.medium_text.initial_position[0]
+          : currentHudRef.current.medium_text.position[0],
+        0.12
+      );
+    }
   });
 
-  const bigHUDPosX = hudElementState.bigHUDPositionX.to(
-    [0, 1],
-    [currentHud.big.initial_position[0], currentHud.big.position[0]]
-  );
+  useEffect(() => {
+    if (activeRef.current) {
+      activeRef.current = false;
+      setTimeout(() => {
+        const hud = getNodeHud(activeNodeMatrixIndices);
+        if (
+          longHudRef.current &&
+          bigHudRef.current &&
+          boringHudRef.current &&
+          greenTextRef.current
+        ) {
+          longHudRef.current.position.y = hud.long.position[1];
+          boringHudRef.current.position.y = hud.boring.position[1];
+          bigHudRef.current.position.y = hud.big.position[1];
+          greenTextRef.current.position.y = hud.medium_text.position[1];
 
-  const boringHUDPosX = hudElementState.boringHUDPositionX.to(
-    [0, 1],
-    [currentHud.boring.initial_position[0], currentHud.boring.position[0]]
-  );
+          longHudRef.current.position.x = hud.long.initial_position[0];
+          boringHudRef.current.position.x = hud.boring.initial_position[0];
+          bigHudRef.current.position.x = hud.big.initial_position[0];
+          greenTextRef.current.position.x = hud.medium_text.initial_position[0];
 
-  const longHUDPosX = hudElementState.longHUDPositionX.to(
-    [0, 1],
-    [currentHud.long.initial_position[0], currentHud.long.position[0]]
-  );
+          if (hud.mirrored) {
+            longHudRef.current.scale.x = -Math.abs(longHudRef.current.scale.x);
+            boringHudRef.current.scale.x = -Math.abs(
+              boringHudRef.current.scale.x
+            );
+            bigHudRef.current.scale.x = -Math.abs(bigHudRef.current.scale.x);
+          } else {
+            longHudRef.current.scale.x = Math.abs(longHudRef.current.scale.x);
+            boringHudRef.current.scale.x = Math.abs(
+              boringHudRef.current.scale.x
+            );
+            bigHudRef.current.scale.x = Math.abs(bigHudRef.current.scale.x);
+          }
+          currentHudRef.current = hud;
+          activeRef.current = true;
+        }
+      }, 500);
+    }
+  }, [activeNodeMatrixIndices]);
 
-  const greenTextPosX = hudElementState.greenTextPositionX.to(
-    [0, 1],
-    [
-      currentHud.medium_text.initial_position[0],
-      currentHud.medium_text.position[0],
-    ]
-  );
+  const longHudRef = useRef<THREE.Object3D>();
+  const boringHudRef = useRef<THREE.Object3D>();
+  const bigHudRef = useRef<THREE.Object3D>();
+  const greenTextRef = useRef<THREE.Object3D>();
 
   const longHudTex = useLoader(THREE.TextureLoader, longHud);
-  const longHudMirroredTex = useLoader(THREE.TextureLoader, longHudMirrored);
-
   const boringHudTex = useLoader(THREE.TextureLoader, boringHud);
-  const boringHudMirroredTex = useLoader(
-    THREE.TextureLoader,
-    boringHudMirrored
-  );
-
   const bigHudTex = useLoader(THREE.TextureLoader, bigHud);
-  const bigHudMirroredTex = useLoader(THREE.TextureLoader, bigHudMirrored);
 
-  const textures = useMemo(
-    () =>
-      currentHud.mirrored === 0
-        ? [longHudTex, boringHudTex, bigHudTex]
-        : [longHudMirroredTex, boringHudMirroredTex, bigHudMirroredTex],
-    [
-      bigHudMirroredTex,
-      bigHudTex,
-      boringHudMirroredTex,
-      boringHudTex,
-      currentHud.mirrored,
-      longHudMirroredTex,
-      longHudTex,
-    ]
-  );
-
+  // console.log("rend");
   return (
     <group position={[0, 0, 10]}>
-      <a.sprite
-        position-x={longHUDPosX}
-        position-y={currentHud.long.position[1]}
-        position-z={currentHud.long.position[2]}
+      <a.mesh
         scale={[1, 0.03, 1]}
         renderOrder={2}
+        ref={longHudRef}
+        position-z={-8.6}
       >
-        <spriteMaterial
+        <planeBufferGeometry attach="geometry" />
+        <meshBasicMaterial
           attach="material"
-          map={textures[0]}
+          map={longHudTex}
           transparent={true}
           depthTest={false}
         />
-      </a.sprite>
-      <a.sprite
-        position-x={boringHUDPosX}
-        position-y={currentHud.boring.position[1]}
-        position-z={currentHud.boring.position[2]}
+      </a.mesh>
+      <a.mesh
         scale={[1, 0.03, 1]}
         renderOrder={2}
+        ref={boringHudRef}
+        position-z={-8.6}
       >
-        <spriteMaterial
+        <planeBufferGeometry attach="geometry" />
+        <meshBasicMaterial
           attach="material"
-          map={textures[1]}
+          map={boringHudTex}
           transparent={true}
           depthTest={false}
         />
-      </a.sprite>
-      <a.sprite
-        position-x={bigHUDPosX}
-        position-y={currentHud.big.position[1]}
-        position-z={currentHud.big.position[2]}
+      </a.mesh>
+      <a.mesh
         scale={[0.5, 0.06, 1]}
         renderOrder={2}
+        ref={bigHudRef}
+        position-z={-8.6}
       >
-        <spriteMaterial
+        <planeBufferGeometry attach="geometry" />
+        <meshBasicMaterial
           attach="material"
-          map={textures[2]}
+          map={bigHudTex}
           transparent={true}
           depthTest={false}
         />
-      </a.sprite>
-      <a.group
-        position-x={greenTextPosX}
-        position-y={currentHud.medium_text.position[1]}
-        position-z={-8.7}
-        scale={[0.02, 0.035, 0.02]}
-      >
-        {greenText.map((letter: string, idx: number) => (
-          <MediumLetter letter={letter} letterIdx={idx} key={idx} />
-        ))}
+      </a.mesh>
+      <a.group position-z={-8.7} scale={[0.02, 0.035, 0.02]} ref={greenTextRef}>
+        <GreenTextRenderer />
       </a.group>
     </group>
   );
---
+++ src/core/utils/lerp.ts
@@ -0,0 +1,3 @@
+const lerp = (x: number, y: number, a: number) => x * (1 - a) + y * a;
+
+export default lerp;
