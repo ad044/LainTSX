aea64c19449c8fd4b663399b3613b3b47fd97058
ad044 <antidepressant044@protonmail.com>
2021-02-11 01:49:14 +0400

    more sounds !: D

--- src/components/KeyPressHandler.tsx
+++ src/components/KeyPressHandler.tsx
@@ -39,6 +39,7 @@ const KeyPressHandler = () => {
       siteManager,
       progressManager,
       mainSubsceneManager,
+      soundManager,
     ],
     []
   );
--- src/static/sfx/index.ts
+++ src/static/sfx/index.ts
@@ -10,6 +10,11 @@ import sound8Audio from "./snd_00008.wav";
 import sound6Audio from "./snd_00006.wav";
 import sound14Audio from "./snd_00014.wav";
 import sound12Audio from "./snd_00012.wav";
+import sound7Audio from "./snd_00007.wav";
+import sound23Audio from "./snd_00023.wav";
+import sound34Audio from "./snd_00034.wav";
+import sound15Audio from "./snd_00015.wav";
+import sound30Audio from "./snd_00030.wav";
 
 const sound0 = new Audio(sound0Audio);
 const sound29 = new Audio(sound29Audio);
@@ -23,6 +28,11 @@ const sound10 = new Audio(sound10Audio);
 const sound6 = new Audio(sound6Audio);
 const sound14 = new Audio(sound14Audio);
 const sound12 = new Audio(sound12Audio);
+const sound7 = new Audio(sound7Audio);
+const sound23 = new Audio(sound23Audio);
+const sound34 = new Audio(sound34Audio);
+const sound15 = new Audio(sound15Audio);
+const sound30 = new Audio(sound30Audio);
 
 export {
   sound0,
@@ -37,4 +47,9 @@ export {
   sound6,
   sound14,
   sound12,
+  sound7,
+  sound23,
+  sound34,
+  sound15,
+  sound30,
 };
