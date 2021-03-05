49df60562a64eddae6982d1375fcf52485f22e8d
elliotcraft79 <16109561+elliotcraft79@users.noreply.github.com>
2021-03-06 01:44:31 +1030

    fix voice extraction

--- scripts/extract/extract_voice.mjs
+++ scripts/extract/extract_voice.mjs
@@ -1,6 +1,6 @@
 import { FileWriter } from "wav";
 import { execSync } from "child_process";
-import { readFileSync, existsSync, mkdirSync } from "fs";
+import { readFileSync, existsSync, mkdirSync, writeFileSync } from "fs";
 import { join } from "path";
 
 export function extract_voice(tempdir, jpsxdec_jar, disc1_index) {
@@ -8,18 +8,30 @@ export function extract_voice(tempdir, jpsxdec_jar, disc1_index) {
     `java -jar ${jpsxdec_jar} -x "${disc1_index}" -i VOICE.BIN -dir "${tempdir}"`,
     { stdio: "inherit" }
   );
-  const voice_files = JSON.parse(readFileSync('voice.json'))
+  const voice_files = JSON.parse(readFileSync("voice.json"));
 
   let voice_data = readFileSync(join(tempdir, "VOICE.BIN"));
   let output_folder = join("..", "..", "src", "static", "voice");
-  if(!existsSync(output_folder)) {
+  if (!existsSync(output_folder)) {
     mkdirSync(output_folder);
   }
 
   for (let voice_file of voice_files) {
     let path = join(output_folder, voice_file.translated_name);
-    let wav_file = new FileWriter(path, {sampleRate: 22050, channels:1, bitDepth: 16});
-    wav_file.write(voice_data.slice(voice_file.offset, voice_file.offset + voice_file.size));
-    wav_file.end();
+    let data = voice_data.slice(
+      voice_file.offset,
+      voice_file.offset + voice_file.size
+    );
+    if (["BYA.WAV", "BYO.WAV", "BYU.WAV"].includes(voice_file.original_name)) {
+      writeFileSync(path, data);
+    } else {
+      let wav_file = new FileWriter(path, {
+        sampleRate: 22050,
+        channels: 1,
+        bitDepth: 16,
+      });
+      wav_file.write(data);
+      wav_file.end();
+    }
   }
 }
